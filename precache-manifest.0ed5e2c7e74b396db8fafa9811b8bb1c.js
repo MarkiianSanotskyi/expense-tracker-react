9cdffca08df11e3200d721df371ac20437be1d56
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-22 14:17:12 -0400

    Updates

---
+++ precache-manifest.0ed5e2c7e74b396db8fafa9811b8bb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d9ec63d9cebf2e0668d1c9c81b8707",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "8113769ac5bf0ff3336a",
+    "url": "/expense-tracker-react/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "ebc37115e32df7a04376",
+    "url": "/expense-tracker-react/static/js/2.5be921a9.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/expense-tracker-react/static/js/2.5be921a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8113769ac5bf0ff3336a",
+    "url": "/expense-tracker-react/static/js/main.1029ecb1.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
