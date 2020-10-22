ad66d6071e7dcf6b0bfedfcbadb3f9132ea54721
Markiian Sanotskyi <marksanockiy@gmail.com>
2020-10-22 13:43:19 -0400

    Done

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer';
 // Initial State
 
 const initialState = {
-    transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-        ]
+    transactions: []
 }
 // Create Context
 
@@ -27,10 +22,18 @@ function deleteTransaction(id) {
     });
 }
 
+function addTransaction(transaction) {
+    dispatch({
+        type: 'ADD_TRANSACTION',
+        payload: transaction
+    });
+}
+
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
-        deleteTransaction
+        deleteTransaction,
+        addTransaction
       }}>
         {children}
       </GlobalContext.Provider>);
